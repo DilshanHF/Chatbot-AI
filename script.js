820c80803abafcbbaa4bf6b160b22c364700ce03
DILSHAN <dilshanfonseka76@gmail.com>
2024-09-24 22:58:17 +0530

    index done

---
+++ script.js
@@ -0,0 +1,3 @@
+const messageBar = document.querySelector(".bar-wrapper input");
+const sendBtn = document.querySelector(".bar-wrapper button");
+const messageBox = document.querySelector(".message-box");
